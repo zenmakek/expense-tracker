65845f7a349a4c489955d3e61672faf4f85dd202
zenmakek <devdhawan81@gmail.com>
2026-05-30 21:58:55 +0530

    add list and delete commands for managing expenses

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.26.3
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.10.2 // indirect
+	github.com/spf13/cobra v1.10.2
 	github.com/spf13/pflag v1.0.9 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"expense-tracker/storage"
 	"fmt"
 	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 )
@@ -26,6 +27,50 @@ var addCmd = &cobra.Command{
 		}
 		fmt.Printf("Expense added! (ID: %d\n)", id)
 
+	},
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all expenses",
+	Run: func(cmd *cobra.Command, args []string) {
+		expenses, err := storage.List()
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		if len(expenses) == 0 {
+			fmt.Println("No expenses found.")
+			return
+		}
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+		fmt.Fprintln(w, "ID\tDate\tDescription\tAmount")
+		fmt.Fprintln(w, "--\t----\t-----------\t------")
+		for _, e := range expenses {
+			fmt.Fprintf(w, "%d\t%s\t%s\t$%.2f\n",
+				e.ID,
+				e.Date.Format("2006-01-02"),
+				e.Description,
+				e.Amount,
+			)
+		}
+		w.Flush()
+	},
+}
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete an expense by ID",
+	Run: func(cmd *cobra.Command, args []string) {
+		id, _ := cmd.Flags().GetInt("id")
+
+		err := storage.Delete(id)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		fmt.Println("Expense deleted successfully")
 	},
 }
 
